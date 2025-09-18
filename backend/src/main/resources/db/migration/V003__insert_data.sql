4605dcdbc301b3b313fd4185df5f57dad5c385f7
Иван Бедяев <ivanbedav1@gmail.com>
2025-09-18 21:06:14 +0300

    Фикс скрипта внесения данных в бд№3

--- backend/src/main/resources/db/migration/V003__insert_data.sql
+++ backend/src/main/resources/db/migration/V003__insert_data.sql
@@ -1,6 +1,4 @@
-INSERT INTO product (id, name, picture_url, price)
-OVERRIDING SYSTEM VALUE  
-VALUES 
+INSERT INTO product (id, name, picture_url, price) VALUES 
     (1, 'Сливочная', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/6.jpg', 320.00),
     (2, 'Особая', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/5.jpg', 179.00),
     (3, 'Молочная', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/4.jpg', 225.00),
